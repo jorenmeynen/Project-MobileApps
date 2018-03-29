5c596c19a9b174f7a99ce6eb6886f92a32ba58e5
Joren Meynen <joren.meynen@telenet.be>
2018-03-29 18:03:07 +0200

    beginsel classes

---
+++ ProjectMobileApps/ProjectMobileApps/ProjectMobileApps/ClassGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectMobileApps
+{
+    class ClassGroup
+    {
+        public int ClassGroup_Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
