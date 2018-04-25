504b36747a0eefe5bdcfea72da97862ea6f7b1e7
Joren Meynen <joren.meynen@telenet.be>
2018-04-25 09:14:18 +0200

    Database one-to-many

--- ProjectMobileApps/ProjectMobileApps/ProjectMobileApps/ClassGroup.cs
+++ ProjectMobileApps/ProjectMobileApps/ProjectMobileApps/ClassGroup.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace ProjectMobileApps
-{
-    class ClassGroup
-    {
-        public int ClassGroupId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+//namespace ProjectMobileApps
+//{
+//    class ClassGroup
+//    {
+//        public int ClassGroupId { get; set; }
+//        public string Name { get; set; }
+//        public string Description { get; set; }
 
-        public ICollection<Corner> Corners { get; set; }
-        public ICollection<Teacher> Teachers { get; set; }
-        public ICollection<Toddler> Toddlers { get; set; }
-    }
-}
+//        public ICollection<Corner> Corners { get; set; }
+//        public ICollection<Teacher> Teachers { get; set; }
+//        public ICollection<Toddler> Toddlers { get; set; }
+//    }
+//}
