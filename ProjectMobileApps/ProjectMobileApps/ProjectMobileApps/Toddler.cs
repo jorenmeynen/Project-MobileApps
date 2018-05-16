bd0407ae7d17fc02e88749825b2070bb62d88a18
Joren Meynen <joren.meynen@telenet.be>
2018-05-16 11:28:04 +0200

    in Toddler.cs : email -> Email

--- ProjectMobileApps/ProjectMobileApps/ProjectMobileApps/Toddler.cs
+++ ProjectMobileApps/ProjectMobileApps/ProjectMobileApps/Toddler.cs
@@ -7,9 +7,9 @@ namespace ProjectMobileApps
     class Toddler
     {
         public int ToddlerId { get; set; }
-        public int ClassGroupId { get; set; }
+        //public int ClassGroupId { get; set; }
         public string Name { get; set; }
-        public string email { get; set; }
+        public string Email { get; set; }
 
         //public ClassGroup ClassGroup { get; set; }
 
