8b2b32746a7b6c2e2350b8310fc1561be80ba516
Leandro Cardinaels <r0447363@ucll.be>
2018-05-23 11:29:44 +0200

    Created ToddlerListPage.

---
+++ ProjectMobileApps/ProjectMobileApps/ProjectMobileApps/Views/ToddlerListPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ProjectMobileApps.Views
+{
+    public partial class ToddlerListPage : ContentPage
+    {
+        public ToddlerListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
