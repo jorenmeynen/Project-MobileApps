fc61e8037ec10ea70aa40d6168373223f38b0024
Joren Meynen <joren.meynen@telenet.be>
2018-05-23 13:52:59 +0200

    changed app icon and name

--- ProjectMobileApps/ProjectMobileApps/ProjectMobileApps.Android/Resources/Resource.designer.cs
+++ ProjectMobileApps/ProjectMobileApps/ProjectMobileApps.Android/Resources/Resource.designer.cs
@@ -3929,7 +3929,13 @@ namespace ProjectMobileApps.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int ic_launcher = 2130903040;
+			public const int cameraatje = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int cameraatje_text = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int ic_launcher = 2130903042;
 			
 			static Mipmap()
 			{
