6b1f61f6cfa9475d8d70b21519a30cedbc1917cd
Joren Meynen <joren.meynen@telenet.be>
2018-05-23 11:27:05 +0200

    page test

--- ProjectMobileApps/ProjectMobileApps/ProjectMobileApps/App.xaml.cs
+++ ProjectMobileApps/ProjectMobileApps/ProjectMobileApps/App.xaml.cs
@@ -31,6 +31,7 @@ namespace ProjectMobileApps
         {
             containerRegistry.RegisterForNavigation<NavigationPage>();
             containerRegistry.RegisterForNavigation<MainPage>();
+            containerRegistry.RegisterForNavigation<MenuPage>();
         }
     }
 }
---
+++ ProjectMobileApps/ProjectMobileApps/ProjectMobileApps/ViewModels/MenuPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+
+namespace ProjectMobileApps.ViewModels
+{
+	public class MenuPageViewModel : ViewModelBase
+	{
+        public MenuPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Menu test Page";
+
+            BackCommand = new DelegateCommand(() =>
+            {
+                NavigationService.GoBackAsync();
+            });
+        }
+        public ICommand BackCommand { get; private set; }
+    }
+}
