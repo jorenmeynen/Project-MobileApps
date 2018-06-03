6f451ee33346ab52357cd2af0af29b2e975a8af9
Joren Meynen <joren.meynen@telenet.be>
2018-06-03 17:46:53 +0200

    Repository

---
+++ ProjectMobileApps/ProjectMobileApps/ProjectMobileApps/Contracts/ICameraatjeRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProjectMobileApps.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMobileApps.Contracts
+{
+    public interface ICameraatjeRepository
+    {
+        Task<List<Toddler>> GetToddlersAsync();
+        Task<List<Corner>> GetCornersAsync();
+        Task<List<Picture>> GetPicturesAsync();
+
+        Task<int> SavePictureAsync(Picture picture);
+    }
+}
---
+++ ProjectMobileApps/ProjectMobileApps/ProjectMobileApps/Repositories/CameraatjeRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectMobileApps.Contracts;
+using ProjectMobileApps.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMobileApps.Repositories
+{
+    public class CameraatjeRepository : ICameraatjeRepository
+    {
+        private ICameraatjeDbContext dbContext;
+
+        public CameraatjeRepository(ICameraatjeDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<List<Toddler>> GetToddlersAsync()
+        {
+            return await dbContext.Toddlers.ToListAsync();
+        }
+
+        public async Task<List<Corner>> GetCornersAsync()
+        {
+            return await dbContext.Corners.ToListAsync();
+        }
+
+        public async Task<List<Picture>> GetPicturesAsync()
+        {
+            return await dbContext.Pictures.ToListAsync();
+        }
+
+        public async Task<int> SavePictureAsync(Picture picture)
+        {
+            if (picture.PictureId == 0)
+            {
+                await dbContext.Pictures.AddAsync(picture);
+            }
+
+            return await dbContext.SaveChangesAsync();
+        }
+    }
+}
