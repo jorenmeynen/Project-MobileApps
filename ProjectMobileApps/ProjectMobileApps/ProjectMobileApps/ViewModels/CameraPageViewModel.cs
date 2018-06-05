c01d28b0f350d1f5cc6dbe359d12ab5552c99087
Leandro Cardinaels <r0447363@ucll.be>
2018-06-06 00:44:31 +0200

    Firebase Authentication en Storage code toegevoegd. Firebase Storage Nuget Package versie veranderd naar 1.0.0

--- ProjectMobileApps/ProjectMobileApps/ProjectMobileApps/ViewModels/CameraPageViewModel.cs
+++ ProjectMobileApps/ProjectMobileApps/ProjectMobileApps/ViewModels/CameraPageViewModel.cs
@@ -1,18 +1,29 @@
-﻿using Plugin.Media;
-using Prism.Commands;
+﻿using Prism.Commands;
+using Plugin.Media;
 using Prism.Mvvm;
 using Prism.Navigation;
 using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using System.Windows.Input;
+using Firebase.Storage;
+using Firebase.Auth;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Plugin.Media.Abstractions;
 
 namespace ProjectMobileApps.ViewModels
 {
 	public class CameraPageViewModel : ViewModelBase
 	{
+        IPageDialogService _dialogService;
+
         public CameraPageViewModel(INavigationService navigationService, IPageDialogService dialogService)
-            :base(navigationService)
+            : base(navigationService)
         {
             Title = "Camera";
 
@@ -20,10 +31,16 @@ namespace ProjectMobileApps.ViewModels
             TakePhotoCommand = new DelegateCommand(TakePhoto);
         }
 
+        private ImageSource source;
+        public ImageSource Source
+        {
+            get { return source; }
+            set { SetProperty(ref source, value); }
+        }
+
         private string _title;
 
         private string _buttonTitle = "Take a Photo";
-        private IPageDialogService _dialogService;
 
         public string ButtonTitle
         {
@@ -71,6 +88,42 @@ namespace ProjectMobileApps.ViewModels
                return stream;
            } 
            ); */
+
+            try
+            {
+                string ApiKey = "AIzaSyBGm7LABs10dXNcZQ5CTeRvuP1zxGUYfDA";
+                string Bucket = "cameraatjefb.appspot.com";
+                string AuthEmail = "joren.meynen@telenet.be";
+                string AuthPassword = "azertyuiop";
+
+
+                // of course you can login using other method, not just email+password
+                var auth = new FirebaseAuthProvider(new FirebaseConfig(ApiKey));
+                var a = await auth.SignInWithEmailAndPasswordAsync(AuthEmail, AuthPassword);
+
+                //Firebase Upload
+                // Constructr FirebaseStorage, path to where you want to upload the file and Put it there
+                var task = new FirebaseStorage(
+                    Bucket,
+                    new FirebaseStorageOptions
+                    {
+                        AuthTokenAsyncFactory = () => Task.FromResult(a.FirebaseToken),
+                    })
+                    .Child("images")
+                    .Child((Convert.ToString(DateTime.Now.ToString("yyyyMMdd-HHmmss")) + ".jpg"))
+                    .PutAsync(file.GetStream());
+
+                // await the task to wait until upload completes and get the download url
+                var downloadUrl = await task;
+                await _dialogService.DisplayAlertAsync("Download Url", downloadUrl, "OK");
+
+                //Firebase download
+                Source = downloadUrl;
+            }
+            catch (Exception ex)
+            {
+                await _dialogService.DisplayAlertAsync("Exception was thrown", ex.Message, "OK");
+            }
         }
     }
 }
